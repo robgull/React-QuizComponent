0f36092034e602c148bd6f7af144a0e3a481ae48
robgull <40916582+robgull@users.noreply.github.com>
2018-12-01 16:29:20 +0000

    Update Quiz.js

--- src/Quiz.js
+++ src/Quiz.js
@@ -6,11 +6,11 @@ class Quiz extends Component
 {
     constructor(props){
         super(props);
-        this.state = {quiz_postition: 1}
+        this.state = {quiz_position: 1}
     }
     render()
     {
-        return <div><div className="QuizQuestions">{quizData.quiz_questions[0].instruction_text}</div></div>
+        return <div><div className="QuizQuestion">{quizData.quiz_questions[0].instruction_text}</div></div>
     }
 }
 export default Quiz;
