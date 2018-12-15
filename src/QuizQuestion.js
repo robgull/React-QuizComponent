4bbcc9a821dbbf5ff948979b743809007b91b5b7
robgull <40916582+robgull@users.noreply.github.com>
2018-12-15 13:49:28 +0000

    remove s

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -25,7 +25,7 @@ class QuizQuestion extends Component
         <section className="buttons">
           <ul>
             {this.props.quiz_question.answer_options.map((answer_options, index)=>{
-              return <QuizQuestion key={index} button_text={answer_options}
+              return <QuizQuestion key={index} button_text={answer_option}
               clickHanlder = {this.handleClick.bind(this)}/>
             })}
           </ul>
