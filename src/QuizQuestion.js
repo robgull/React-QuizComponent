23bf779420ef42e9a328b758e16f78c00e94bc4e
robgull <40916582+robgull@users.noreply.github.com>
2018-12-15 13:45:14 +0000

    add stuff

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import QuizQuestionButton from './QuizQuestionButton.js';
+import Quiz from './Quiz.js';
 
 class QuizQuestion extends Component
 {
@@ -8,15 +9,25 @@ class QuizQuestion extends Component
         super(props);
 
     }
+    handleClick(buttonText)
+    {
+      if (buttonText === this.props.quiz_question.answer){
+        this.props.showNextQuestionHandler(); 
+      }
+    }    
+
     render()
     {
-        return <main>
+    return <main>
         <section>
           <p>{this.props.quiz_question.instruction_text}</p>
         </section>
         <section className="buttons">
           <ul>
-            <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]}/>
+            {this.props.quiz_question.answer_options.map((answer_options, index)=>{
+              return <QuizQuestion key={index} button_text={answer_options}
+              clickHanlder = {this.handleClick.bind(this)}/>
+            })}
           </ul>
         </section>
       </main>
