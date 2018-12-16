3fcc4fc6d72c91c4a9796887bc9d09b9adfd0f1e
robgull <40916582+robgull@users.noreply.github.com>
2018-12-16 13:21:59 +0000

    add error message

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -7,12 +7,16 @@ class QuizQuestion extends Component
     constructor(props)
     {
         super(props);
-
+        this.state = {incorrectAnswer : false}
     }
     handleClick(buttonText)
     {
       if (buttonText === this.props.quiz_question.answer){
+        this.setState({incorrectAnswer : false});
         this.props.showNextQuestionHandler(); 
+      }
+      else{
+        this.setState({incorrectAnswer : true});
       }
     }    
 
@@ -29,6 +33,7 @@ class QuizQuestion extends Component
             })}
           </ul>
         </section>
+        {this.state.incorrectAnswer ? <p className="error">Sorry that's not right</p> : null}
       </main>)
     }
 }
